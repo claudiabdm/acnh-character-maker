955b4fb2a84e42d5a47c83dc17ff0cdb24f323d4
claudiabdm <claudiabdm@hotmail.es>
2021-06-05 23:35:57 +0200

    Add icons for pwa
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

--- vue.config.ts
+++ vue.config.ts
@@ -1,4 +1,18 @@
 module.exports = {
   // publicPath: '/'
   // assetsDir: './assets'
+  pwa: {
+    name: 'Animal Crossing: Character Maker',
+    shortName: 'AC: Char Maker',
+    description: 'Create your animal crossing character! Built with Vue 3',
+    backgroundColor: '#eceed6',
+    themeColor: '#49dbc6',
+    msTileColor: '#49dbc6',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
+  }
 };
