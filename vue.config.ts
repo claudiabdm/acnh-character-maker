dffb02a83e8a2ac7d3054a37f424e2bd131351cb
claudiabdm <claudiabdm@hotmail.es>
2021-06-06 12:11:28 +0200

    Fix pwa config
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

--- vue.config.ts
+++ vue.config.ts
@@ -2,12 +2,14 @@ module.exports = {
   // publicPath: '/'
   // assetsDir: './assets'
   pwa: {
-    name: 'Animal Crossing: Character Maker',
-    shortName: 'AC: Char Maker',
-    description: 'Create your animal crossing character! Built with Vue 3',
-    backgroundColor: '#eceed6',
-    themeColor: '#49dbc6',
-    msTileColor: '#49dbc6',
+    manifestOptions: {
+      name: 'Animal Crossing: Character Maker',
+      shortName: 'AC: Char Maker',
+      description: 'Create your animal crossing character! Built with Vue 3',
+      backgroundColor: '#eceed6',
+      themeColor: '#49dbc6',
+      msTileColor: '#49dbc6'
+    },
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
