354e53b0c1f6625204218fb16cd808d4bbdbfaaa
claudiabdm <claudiabdm@hotmail.es>
2024-02-03 20:47:37 +0100

    Change import path

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,37 +13,37 @@ const routes: Array<RouteRecordRaw> = [
       {
         path: 'skin',
         name: 'skin',
-        component: () => import('../views/SettingsSkin.vue')
+        component: () => import('@/views/SettingsSkin.vue')
       },
       {
         path: 'hair',
         name: 'hair',
-        component: () => import('../views/SettingsHair.vue')
+        component: () => import('@/views/SettingsHair.vue')
       },
       {
         path: 'eyes',
         name: 'eyes',
-        component: () => import('../views/SettingsEyes.vue')
+        component: () => import('@/views/SettingsEyes.vue')
       },
       {
         path: 'nose-mouth',
         name: 'nose-mouth',
-        component: () => import('../views/SettingsNoseMouth.vue')
+        component: () => import('@/views/SettingsNoseMouth.vue')
       },
       {
         path: 'blush',
         name: 'blush',
-        component: () => import('../views/SettingsBlush.vue')
+        component: () => import('@/views/SettingsBlush.vue')
       },
       {
         path: 'background',
         name: 'background',
-        component: () => import('../views/SettingsBackground.vue')
+        component: () => import('@/views/SettingsBackground.vue')
       },
       {
         path: 'clothes',
         name: 'clothes',
-        component: () => import('../views/SettingsClothes.vue')
+        component: () => import('@/views/SettingsClothes.vue')
       }
     ]
   }, 
