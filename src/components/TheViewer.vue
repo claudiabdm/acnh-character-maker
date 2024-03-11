35e4a57362775c177a56d581cfd58185dfb46318
claudiabdm <claudiabdm@hotmail.es>
2024-03-11 14:13:30 +0100

    Disable google ads

--- src/components/TheViewer.vue
+++ src/components/TheViewer.vue
@@ -29,10 +29,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheCharacter from '@/components/TheCharacter.vue';
-import GoogleAd from '@/components/GoogleAd.vue';
 
 export default defineComponent({
-  components: { TheCharacter, GoogleAd },
+  components: { TheCharacter },
   data() {
     return {
       downloadText: 'Download Image'
